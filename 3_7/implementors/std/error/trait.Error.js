17e47abcd57a612bb6ea51c97405855e08312425
Kyle Mayes <kyle@mayeses.com>
2017-07-02 17:23:04 -0400

    "Update documentation"

---
+++ 3_7/implementors/std/error/trait.Error.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["glob"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glob/struct.GlobError.html\" title=\"struct glob::GlobError\">GlobError</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glob/struct.PatternError.html\" title=\"struct glob::PatternError\">PatternError</a>",];
+implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
